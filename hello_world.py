4abdaa7c46b094628b433e19388bb214e45bfc0e
Aishwarya0206 <aishustarz02@gmail.com>
2022-01-16 10:09:01 +0530

    Modifications made in hello_world file

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,6 @@
-print("Hello world")
+import pandas as pd
+dictionary = {'a': [1,2,3,4], 'b': [5,6,7,8], 'c': [9, 10, 11, 12], 'd': [13, 14, 15, 16], 'e': [17,18,19,20], 'f': [21,22,23,24]}
+df = pd.DataFrame(dictionary)
+print(df.head())
+for x, y in dictionary.items():
+    print(x, y)
